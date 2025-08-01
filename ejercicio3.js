2350ecf9e10d36b62f73efa77c6f11b4dc45b575
aichitame <159391105+aichitame@users.noreply.github.com>
2025-08-01 16:31:48 +0200

    ejercicio 3 hecho

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,3 +1,5 @@
+//Dado el siguiente código usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito.Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos. Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+
 const users = [
     {
       name: "Alberto",
@@ -32,3 +34,17 @@ const users = [
       },
     },
   ];
+
+let vecesFavorito = {};
+
+for(const user of users){
+    for (const sound in user.favoritesSounds){
+        if(vecesFavorito[sound]){
+            vecesFavorito[sound]++;
+        } else{
+            vecesFavorito[sound] = 1;
+        }
+        
+    }
+}
+console.log(vecesFavorito);
